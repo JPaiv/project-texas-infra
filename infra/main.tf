68405e7415096a4c547138476bfb614ada13e21f
JPaiv <juho.paivarinta@outlook.com>
2022-01-03 06:26:53 +0200

    Remove version constraints.

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.5.0"
     }
   }
   backend "gcs" {
@@ -12,14 +11,12 @@ terraform {
 }
 
 # provider "google" {
-#   version     = "4.5.0"
 #   credentials = "${file(var.credentials)}"
 #   project     = var.project_id
 #   region      = var.regionte
 # }
 
 provider "google-beta" {
-  version     = "4.5.0"
   credentials = "${file(var.credentials)}"
   project     = var.project_id
   region      = var.region
