935226617f9d181dc77911f5c7b52fdcd332771d
JPaiv <juho.paivarinta@outlook.com>
2022-01-06 01:29:53 +0200

    Use container registry.

--- infra/modules/registry/artifact_registry.tf
+++
@@ -1,8 +0,0 @@
-resource "google_artifact_registry_repository" "artifact_repository" {
-  provider = google-beta
-
-  location      = "eu-central1"
-  repository_id = "${var.environment}-artifact-repo"
-  description   = "Docker test repo"
-  format        = "DOCKER"
-}
