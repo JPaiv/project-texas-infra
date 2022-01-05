7abea97b875d2e9e4adb619fdf7e8148c2add164
JPaiv <juho.paivarinta@outlook.com>
2022-01-06 01:01:33 +0200

    Deploy container repository.

--- infra/main.tf
+++ infra/main.tf
@@ -10,23 +10,23 @@ terraform {
   }
 }
 
-module "vpc" {
-  source      = "./modules/vpc"
-  region      = var.region
-  environment = var.environment
-}
-
-module "gke" {
-  source      = "./modules/gke"
-  region      = var.region
-  project_id  = var.project_id
-  environment = var.environment
-  vpc_name    = module.vpc.vpc_id
-  subnet_name = module.vpc.subnet_id
-}
-
-# module "registry" {
-#   source          = "./modules/registry"
-#   project_id      = var.project_id
-#   service_account = var.service_account
+# module "vpc" {
+#   source      = "./modules/vpc"
+#   region      = var.region
+#   environment = var.environment
 # }
+
+# module "gke" {
+#   source      = "./modules/gke"
+#   region      = var.region
+#   project_id  = var.project_id
+#   environment = var.environment
+#   vpc_name    = module.vpc.vpc_id
+#   subnet_name = module.vpc.subnet_id
+# }
+
+module "registry" {
+  source          = "./modules/registry"
+  project_id      = var.project_id
+  service_account = var.service_account
+}
