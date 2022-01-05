30bb072ff228055ede9d8822f84362ede6c65fcd
JPaiv <juho.paivarinta@outlook.com>
2022-01-06 01:13:52 +0200

    Remove unneeded comments.

--- infra/modules/gke/gke.tf
+++ infra/modules/gke/gke.tf
@@ -2,9 +2,6 @@ resource "google_container_cluster" "primary_cluster" {
   name     = "${var.environment}-project-texas-cluster"
   location = var.region
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
 
@@ -12,7 +9,6 @@ resource "google_container_cluster" "primary_cluster" {
   subnetwork = var.subnet_name
 }
 
-# Separately Managed Node Pool
 resource "google_container_node_pool" "primary_node_pool" {
   name       = "${var.environment}-primary-cluster-node-pool"
   location   = var.region
