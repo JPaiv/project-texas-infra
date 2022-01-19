cf1705cd49eebe2994d92d78ffbc8ec8e6d0ba84
JPaivFiare <juho.paivarinta@fiare.com>
2022-01-19 02:35:54 +0200

    Update hello welcome message.

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app.config["DEBUG"] = True
 
 @app.route('/hello', methods=['GET'])
 def hello_world_api():
-    return jsonify("Fiare is the best IT company!")
+    return jsonify("Task has been completed successfully!")
 
 
 @app.route('/vowel-service', methods=['POST'])
