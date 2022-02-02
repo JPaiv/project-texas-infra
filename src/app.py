1c749535c89101696c747bcdb800983983d75d2e
JPaiv <juho.paivarinta@outlook.com>
2022-02-02 13:47:36 +0200

    Update application for demo.

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app.config["DEBUG"] = True
 
 @app.route('/hello', methods=['GET'])
 def hello_world_api():
-    return jsonify("Task has been completed successfully!")
+    return jsonify("Task has been updated successfully during interview!")
 
 
 @app.route('/vowel-service', methods=['POST'])
